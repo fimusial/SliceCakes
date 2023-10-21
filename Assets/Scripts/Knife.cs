99e10b4755290c7eadbbab6b9edbdfb8eca55adf
Filip Musiał <fimusial@gmail.com>
2023-10-21 20:26:54 +0200

    added left handed knife option

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -5,12 +5,18 @@ public class Knife : MonoBehaviour
   public string sliceAnimationTriggerName = "KnifeSliceTrigger";
   public string sliceAnimationStateName = "KnifeSlice";
   public int sliceAnimationLayerIndex = 0;
+  public bool leftHanded = false;
 
   private Animator animator;
 
   public void Start()
   {
-    animator = gameObject.GetComponent<Animator>();
+    animator = gameObject.GetComponentInChildren<Animator>();
+
+    if (leftHanded)
+    {
+      transform.Rotate(0f, 180f, 0f);
+    }
   }
 
   public void TriggerSliceAnimation()
