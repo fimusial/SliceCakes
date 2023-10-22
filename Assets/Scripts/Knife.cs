9cfb8c6b296db756447ea82cf47d41e2bfcc3858
Filip Musiał <fimusial@gmail.com>
2023-10-22 14:18:23 +0200

    variable slice angle + refactor

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -5,18 +5,19 @@ public class Knife : MonoBehaviour
   public string sliceAnimationTriggerName = "KnifeSliceTrigger";
   public string sliceAnimationStateName = "KnifeSlice";
   public int sliceAnimationLayerIndex = 0;
-  public bool leftHanded = false;
 
   private Animator animator;
+
+  public float SliceAtAngle { get; set; } = 0f;
 
   public void Start()
   {
     animator = gameObject.GetComponentInChildren<Animator>();
+  }
 
-    if (leftHanded)
-    {
-      transform.Rotate(0f, 180f, 0f);
-    }
+  public void Update()
+  {
+    transform.rotation = Quaternion.AngleAxis(SliceAtAngle, Vector3.up);
   }
 
   public void TriggerSliceAnimation()
